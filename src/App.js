64585fac1cb3939603560ecac86b957ec2f89919
Tony Porter <tonyporter89@gmail.com>
2019-09-01 15:09:31 +0100

    removed mock form, rendered mock data from Contract.js module

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
+import Contracts from "./Contracts";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Contract Reminder</h1>
-      <form>
-        <label>
-          Contract
-          <input type="text" name="contract" />
-        </label>
-        <label>
-          End Date
-          <input type="" name="endDate" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contracts: [
+      { id: 1, contract: "Sky", endDate: "01-01-2020" },
+      { id: 2, contract: "EE", endDate: "02-01-2020" }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Contract Reminder</h1>
+        <Contracts contracts={this.state.contracts} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Contracts.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Contracts = ({ contracts }) => {
+  const contractsList = contracts.length ? (
+    contracts.map((contract) => {
+      return (
+        <div key={contract.id}>
+          <span>{contract.contract}</span>
+          <span>{contract.endDate}</span>
+        </div>
+      );
+    })
+  ) : (
+    <p className="noContracts">You have no contracts</p>
+  );
+  return <div className="contracts">{contractsList}</div>;
+};
+
+export default Contracts;
